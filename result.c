1e5ba6b2215a1e83cab34cfa42b566a2267d0fd0
Ojasviji <110409735+Ojasviji@users.noreply.github.com>
2022-08-05 12:11:13 +0530

    result add

---
+++ result.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("name\tram");
+    printf("\nclass\tbca");
+    printf("\nresult\tpass");
+    printf("\ngrade\tA");
+    printf("\ncontact\t 9000000001");
+    return 0;
+}
